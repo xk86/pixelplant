34e85019a1d162b574285274c5af16ae68e47541
Cat Dixon <xk86d64@gmail.com>
2025-05-01 11:44:12 -0700

    update eslint config and add react+react hooks linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import eslint from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  react.configs.recommended,
-  reactHooks.configs.recommended,
+  ...tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
+  react.configs.flat.recommended, // React recommended rules
+  react.configs.flat["jsx-runtime"], // Add this if using React 17+ or the new JSX transform
+  {
+    // React Hooks rules
+    files: ["**/*.jsx", "**/*.tsx"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+  },
 ];
