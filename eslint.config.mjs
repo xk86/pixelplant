61e2d2f3ac33852f75ec9818c75a34fafa5b732a
Cat Dixon <xk86d64@gmail.com>
2025-05-01 11:16:09 -0700

    install and set up eslint for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default [
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs.recommended,
+];
